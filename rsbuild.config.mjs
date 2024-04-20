bc11e8a4f0908b48b42c624bb8d5986940efe32b
nexx <670435363@qq.com>
2024-04-20 13:12:32 +0000

    提交代码

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,6 +1,33 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
 
 export default defineConfig({
   plugins: [pluginReact()],
+
+  output: {
+    cssModules: {
+      auto: (resource) => {
+        let bl = resource.indexOf("nexx-test-less") > -1;
+        console.log("bl:", bl, resource);
+        return bl;
+      },
+    },
+  },
+
+  tools: {
+    rspack(config) {
+      config.module.parser["css/module"] = {
+        namedExports: false,
+      };
+      config.module.parser["css/auto"] = {
+        namedExports: true,
+      };
+      config.module.parser["css"] = {
+        namedExports: true,
+      };
+      config.experiments.css = true;
+
+      return config;
+    },
+  },
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import './App.css';
+import "./App.css";
+import Test from "nexx-test-less";
 
 const App = () => {
   return (
-    <div className="content">
+    <div className={`content`}>
+      <Test>Test</Test>
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
     </div>
